c8a406f994d3feb412662f9ee7467d8bb93b37ad
iamrjb11 <iamrjb11@gmail.com>
2019-12-11 23:10:30 +0600

    updated

--- api/routes/test.js
+++ api/routes/test.js
@@ -1,23 +1,109 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Userdb = require('../models/userdb');
+
+router.get('/all', (req, res, next) => {
+    Userdb.find()
+        .exec()
+        .then(results => {
+            if (results.length > 0) {
+                res.status(200).json({
+                    message: "All User Info",
+                    result: results
+                });
+            } else {
+                res.status(404).json({
+                    message: "All User Info",
+                    result: 'No data available'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+});
+router.get('/:userId', (req, res, next) => {
+    //Console.log("INNN");\
+    const id = req.params.userId;
+    Userdb.findById(id)
+        .exec()
+        .then(result => {
+            console.log(result);
+            if (result)
+                res.status(200).json(result);
+            else
+                res.status(404).json({ message: 'orry, your  userid is not valid!' });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 
 
-router.get('/', (req, res, next) => {
-    //Console.log("INNN");
-    res.status(200).json({
-        message: 'Its a GET method'
-    });
 });
 
 router.post('/', (req, res, next) => {
-    const user = {
+    // const user =  {
+    //     name: req.body.name,
+    //     age: req.body.age
+    // }
+    const user = new Userdb({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         age: req.body.age
-    }
+    });
+    user.save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => console.log(err));
+
     res.status(200).json({
         message: 'Its a POST method',
         user: user
     });
+});
+router.delete('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    Userdb.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Deleted',
+                result: result,
+
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.patch('/:userId', (req, res, next) => {
+    const id = req.params.userId;
+    Userdb.update({ _id: id }, { $set: { name: req.body.newname, age: newage } })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Updated',
+                result: result
+            });
+        })
+        .catch(err => {
+            res.status(404).json({
+                message: 'Error !',
+                error: err
+            })
+        })
+
+    ;
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-
+const mongoose = require('mongoose');
 const testR = require('./api/routes/test');
+mongoose.connect('mongodb+srv://iamrjb:iamrjb11@cluster0-lu43k.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true },
+    console.log("Connected")
+
+)
+
 app.use(bodyParser.json());
 
 app.use('/test', testR);
